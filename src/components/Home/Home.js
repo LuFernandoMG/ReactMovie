66753d8f567ddc33d3226cea143496993a893236
Luis Fernando Mendez <luisfernando.mendez5@gmail.com>
2020-08-11 22:24:51 -0400

    Component solve issue

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { API_URL, API_KEY, IMAGE_BASE_URL, POSTER_SIZE, BACKDROP_SIZE } from '../../config';
 import HeroImage from '../elements/HeroImage/HeroImage';
 import SearchBar from '../elements/SearchBar/SearchBar';
